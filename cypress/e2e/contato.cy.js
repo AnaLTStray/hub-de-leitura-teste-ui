0db4274846891ae2fd4c8036c41420025ecb102a
Ana Letícia Travaglia Stray <anastray@hotmail.com>
2026-04-24 09:56:18 -0300

    Automação do Contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,52 @@
+describe('Funcionalidade: Contato', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/index.html')
+  });
+
+  it('Deve preencher formulário de contato com sucesso', () => {
+    cy.get( '#name').type('Ana Letícia')
+    cy.get ('#email').type('anateste@teste.com')
+    cy.get ('#subject').select('Sugestões')
+    cy.get ('#message').type('Esta é uma mensagem de teste.')
+    cy.get ('#btn-submit').click()
+    cy.contains('Contato enviado com sucesso!').should('exist')
+  });
+  
+    it('Deve validar mensagem de erro ao enviar formulário sem preencher nome', () => {   
+    cy.get( '#name').clear
+    cy.get('#email').type('anateste@teste.com')
+    cy.get('#subject').select('Sugestões')
+    cy.get('#message').type('Esta é uma mensagem de teste.')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should ('contain', 'Por favor, preencha o campo Nome.')
+  });
+
+    it('Deve validar mensagem de erro ao enviar formulário sem preencher e-mail', () => {
+    cy.get( '#name').type('Ana Letícia')
+    cy.get('#email').clear
+    cy.get('#subject').select('Sugestões')
+    cy.get('#message').type('Esta é uma mensagem de teste.')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should ('contain', 'Por favor, preencha o campo E-mail.')
+  });
+
+    it('Deve validar mensagem de erro ao enviar formulário sem selecionar o assunto', () => {
+    cy.get( '#name').type('Ana Letícia')
+    cy.get('#email').type('anateste@teste.com')
+    cy.get('#subject').clear
+    cy.get('#message').type('Esta é uma mensagem de teste.')
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should ('contain', 'Por favor, selecione o Assunto.')
+  });
+
+    it('Deve validar mensagem de erro ao enviar formulário sem selecionar a mensagem', () => {
+    cy.get( '#name').type('Ana Letícia')
+    cy.get('#email').type('anateste@teste.com')
+    cy.get('#subject').select('Sugestões')
+    cy.get('#message').clear
+    cy.get('#btn-submit').click()
+    cy.get('#alert-container').should ('contain', 'Por favor, escreva sua Mensagem.')
+  });
+
+})
